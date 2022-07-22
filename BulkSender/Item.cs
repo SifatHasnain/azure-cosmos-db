4ba11f3da9a909eb1f88eee2ca752f198cc62cf0
bsse1009 <mik858692@gmail.com>
2022-07-22 23:43:43 +0600

    Add dotnet bulk sender from json file

---
+++ BulkSender/Item.cs
@@ -0,0 +1,22 @@
+public class Item
+    {
+        public string id;
+        public string AbrId;
+        public string Abn;
+        public string EntityType;
+        public int ASICNumber;
+        public string Acn;
+        public string PostCode;
+        public string EntityName;
+        public string BusinessName;
+        public string TradingName;
+        public string State;
+        public int TradingCount;
+        public int BusinessCount;
+        public string Active;
+        public bool AbnStatus;
+        public bool AcnStatus;
+        public DateTime LastUpdateDate;
+    }
+
+                    
---
+++ BulkSender/Readme.md
@@ -0,0 +1,5 @@
+# Run and Build
+
+> dotnet run <apiPath> <apiKey> <jsonPath>
+
+> dotnet run "https://cosmos-db-test1-serverless.documents.azure.com:443/" "LLmkBIfqsclH5vEta5ouBXn03HibZhw77HRqaMFk3STfNzrEcFHb4EAA3lP7mk2CLluSvulyZ9DvFZbfnqxNvQ==" "gen.json"
